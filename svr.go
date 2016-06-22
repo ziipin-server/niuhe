f2877d1004c8aebbd71b5efceb0cd0fb3de72dd6
howcrazy <crazy@1qi.in>
2016-06-22 11:48:50 +0800

    append function PathPrefix to niuhe.Server

--- svr.go
+++ svr.go
@@ -1,19 +1,32 @@
 package niuhe
 
 import (
-	"github.com/gin-gonic/gin"
+	"fmt"
 	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
 )
 
 type Server struct {
-	modules []*Module
-	engine *gin.Engine
+	PathPrefix string
+	modules    []*Module
+	engine     *gin.Engine
 }
 
 func NewServer() *Server {
 	return &Server{
 		modules: make([]*Module, 0),
 	}
+}
+
+func (svr *Server) SetPathPrefix(prefix string) {
+	if prefix != "" {
+		if !strings.HasSuffix(prefix, "/") {
+			prefix = fmt.Sprintf("%s/", prefix)
+		}
+	}
+	svr.PathPrefix = prefix
 }
 
 func (svr *Server) RegisterModule(mod *Module) {
@@ -29,7 +42,7 @@ func (svr *Server) GetGinEngine() *gin.Engine {
 		svr.engine = gin.New()
 		svr.engine.Use(gin.LoggerWithWriter(os.Stderr), gin.Recovery())
 		for _, mod := range svr.modules {
-			group := svr.engine.Group(mod.urlPrefix)
+			group := svr.engine.Group(svr.PathPrefix + mod.urlPrefix)
 			for _, info := range mod.handlers {
 				if (info.Methods & GET) != 0 {
 					group.GET(info.Path, info.handleFunc)
