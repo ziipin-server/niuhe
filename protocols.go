7a01f42730c456e8651e8092ec17f44afb896785
lennon-guan <i@guanming.me>
2016-11-01 11:17:43 +0800

    fix bug

--- protocols.go
+++ protocols.go
@@ -8,8 +8,12 @@ type jsonApiProtocol struct {
 	DefaultApiProtocol
 }
 
-func (self jsonApiProtocol) Read(c *Context, reqValue reflect.Value) error {
+func (self *jsonApiProtocol) Read(c *Context, reqValue reflect.Value) error {
 	return c.BindJSON(reqValue.Interface())
+}
+
+func (self *jsonApiProtocol) Write(c *Context, rsp reflect.Value, err error) error {
+	return self.DefaultApiProtocol.Write(c, rsp, err)
 }
 
 var JsonApiProtocolFactory = ApiProtocolFactoryFunc(func() IApiProtocol {
