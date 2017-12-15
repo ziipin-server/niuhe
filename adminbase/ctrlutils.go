4101000721324180f3a7685b2f8419dbf8cc965e
guanming <i@guanming.me>
2017-12-15 12:45:03 +0800

    fix bugs

--- adminbase/ctrlutils.go
+++ adminbase/ctrlutils.go
@@ -2,12 +2,7 @@ package adminbase
 
 import (
 	"reflect"
-	"regexp"
-	"strings"
-)
-
-var (
-	pascalRe *regexp.Regexp
+	"unicode"
 )
 
 func MakeColRenderer(modelField, colField string) ColRenderer {
@@ -22,8 +17,53 @@ func MakeSameRenderer(modelField string) ColRenderer {
 }
 
 func pascalToSnake(pascalName string) string {
-	parts := pascalRe.FindAllString(pascalName, -1)
-	return strings.ToLower(strings.Join(parts, "_"))
+	in := []rune(pascalName)
+	out := make([]rune, 0, 2*len(in))
+	hasPrefixUpper := false
+	isLastLetter := false
+	var prefixBegin int
+	for i, ch := range in {
+		if unicode.IsUpper(ch) {
+			if !hasPrefixUpper {
+				prefixBegin = i
+				hasPrefixUpper = true
+			}
+			isLastLetter = true
+		} else if unicode.IsLetter(ch) {
+			if hasPrefixUpper {
+				if prefixBegin > 0 {
+					out = append(out, rune('_'))
+				}
+				prefixEnd := i - 1
+				if prefixBegin < prefixEnd {
+					for j := prefixBegin; j < prefixEnd; j++ {
+						out = append(out, unicode.ToLower(in[j]))
+					}
+					out = append(out, rune('_'))
+				}
+				out = append(out, unicode.ToLower(in[prefixEnd]))
+				hasPrefixUpper = false
+			}
+			out = append(out, ch)
+			isLastLetter = true
+		} else {
+			if isLastLetter {
+				out = append(out, rune('_'))
+			}
+			isLastLetter = false
+			out = append(out, ch)
+		}
+	}
+	if hasPrefixUpper {
+		prefixEnd := len(in)
+		if prefixBegin > 0 {
+			out = append(out, rune('_'))
+		}
+		for j := prefixBegin; j < prefixEnd; j++ {
+			out = append(out, unicode.ToLower(in[j]))
+		}
+	}
+	return string(out)
 }
 
 func MakeSnakeRenderers(model interface{}, skipModelFields ...string) []ColRenderer {
@@ -52,9 +92,5 @@ func MakeSnakeRenderers(model interface{}, skipModelFields ...string) []ColRende
 		}
 		renderers = append(renderers, MakeColRenderer(fname, pascalToSnake(fname)))
 	}
-	return nil
-}
-
-func init() {
-	pascalRe = regexp.MustCompile("[A-Z][a-z]*|[^A-Za-z]+")
+	return renderers
 }
--- adminbase/ctrlutils_test.go
+++ adminbase/ctrlutils_test.go
@@ -1,9 +1,21 @@
 package adminbase
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestPascalToSnake(t *testing.T) {
 	if orig := "AppId"; pascalToSnake(orig) != "app_id" {
+		t.Error("pascalToSnake error", orig, pascalToSnake(orig))
+	}
+	if orig := "AppID"; pascalToSnake(orig) != "app_id" {
+		t.Error("pascalToSnake error", orig, pascalToSnake(orig))
+	}
+	if orig := "ID"; pascalToSnake(orig) != "id" {
+		t.Error("pascalToSnake error", orig, pascalToSnake(orig))
+	}
+	if orig := "IPAddr"; pascalToSnake(orig) != "ip_addr" {
 		t.Error("pascalToSnake error", orig, pascalToSnake(orig))
 	}
 	if orig := "Name"; pascalToSnake(orig) != "name" {
@@ -14,5 +26,33 @@ func TestPascalToSnake(t *testing.T) {
 	}
 	if orig := "UserName1984"; pascalToSnake(orig) != "user_name_1984" {
 		t.Error("pascalToSnake error", orig, pascalToSnake(orig))
+	}
+}
+
+type account struct {
+	ID             int64     `xorm:"'id' notnull pk autoincr"`
+	Username       string    `xorm:"'username' varchar(15)"`
+	Password       string    `xorm:"'password' varchar(60)"`
+	MachineID      string    `xorm:"'machine_id' varchar(40)"`
+	Status         int       `xorm:"'status' notnull"`
+	Coins          int64     `xorm:"'coins'"`
+	Diamond        int       `xorm:"'diamond' notnull"`
+	Money          int       `xorm:"'money' notnull"`
+	Nickname       string    `xorm:"'nickname' varchar(50) notnull"`
+	PhoneNumber    string    `xorm:"'phone_number' varchar(20)"`
+	Icon           string    `xorm:"'icon' varchar(100) notnull"`
+	Gender         int       `xorm:"'gender' notnull"`
+	RegistrationID string    `xorm:"'registration_id' varchar(40) notnull"`
+	ChannelName    string    `xorm:"'channel_name' varchar(128) notnull"`
+	SourceChannel  string    `xorm:"'source_channel' varchar(128) notnull"`
+	Created        time.Time `xorm:"'created' DATETIME notnull"`
+	Updated        time.Time `xorm:"'updated' DATETIME notnull"`
+}
+
+func TestMakeSnakeRenderers(t *testing.T) {
+	m := new(account)
+	r := MakeSnakeRenderers(m, "Password")
+	if len(r) != 16 {
+		t.Error("len error", r)
 	}
 }
