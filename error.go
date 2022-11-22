29e70eb0fed8fa4aa3d07da009e28643b7485df9
冯家耀 <jiayaf@gmail.com>
2022-11-22 16:32:34 +0800

    panic protect: support (*CommError) nil method

--- error.go
+++ error.go
@@ -21,14 +21,23 @@ func NewCommError(code int, message string) *CommError {
 }
 
 func (err *CommError) GetCode() int {
+	if nil == err {
+		return 0
+	}
 	return err.Code
 }
 
 func (err *CommError) GetMessage() string {
+	if nil == err {
+		return ""
+	}
 	return err.Message
 }
 
 func (err *CommError) Error() string {
+	if nil == err {
+		return ""
+	}
 	return fmt.Sprintf("%d:%s", err.Code, err.Message)
 }
 
